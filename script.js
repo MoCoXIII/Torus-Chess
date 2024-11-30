5561eaaeb83dd4c85449809cb3d35a8f568cca41
MoCoXIII <moritzflynnkramer@gmail.com>
2024-11-30 23:25:16 +0100

    Version Changed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById('version').textContent = 'Version 0.2024.11.30.23.20.x';
+document.getElementById('version').textContent = 'Version 0.2024.11.30.23.25.x';
 
 window.addEventListener('load', () => {
   const windowSize = window.innerWidth < window.innerHeight ? window.innerWidth : window.innerHeight;
