93c5f71b826bbfc5ed4f9009d3b6a26ebb62feca
MoCoXIII <moritzflynnkramer@gmail.com>
2024-11-30 23:06:12 +0100

    Better Resizing Now also respects height when monitor is wide

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 document.getElementById('version').textContent = 'Version 0.2024.11.30.23.x';
 
 window.addEventListener('load', () => {
-  const windowWidth = window.innerWidth;
+  const windowSize = window.innerWidth < window.innerHeight ? window.innerWidth : window.innerHeight;
+
   const chessboard = document.getElementById('chessboard');
-  chessboard.style.width = `${windowWidth * 0.8}px`;
-  chessboard.style.height = `${windowWidth * 0.8}px`;
+  chessboard.style.width = `${windowSize * 0.8}px`;
+  chessboard.style.height = `${windowSize * 0.8}px`;
   const squares = document.querySelectorAll('.square');
   squares.forEach(square => {
-    square.style.width = `${windowWidth * 0.8 / 8}px`;
-    square.style.height = `${windowWidth * 0.8 / 8}px`;
+    square.style.width = `${windowSize * 0.8 / 8}px`;
+    square.style.height = `${windowSize * 0.8 / 8}px`;
   });
   const pieces = document.querySelectorAll('.piece');
   pieces.forEach(piece => {
-    piece.style.fontSize = `${windowWidth * 0.8 / 8 / 2}px`;
-    piece.style.lineHeight = `${windowWidth * 0.8 / 8}px`;
-    piece.style.width = `${windowWidth * 0.8 / 8}px`;
-    piece.style.height = `${windowWidth * 0.8 / 8}px`;
+    piece.style.fontSize = `${windowSize * 0.8 / 8 / 2}px`;
+    piece.style.lineHeight = `${windowSize * 0.8 / 8}px`;
+    piece.style.width = `${windowSize * 0.8 / 8}px`;
+    piece.style.height = `${windowSize * 0.8 / 8}px`;
   });
-  chessboard.style.gridTemplateRows = `repeat(8, ${windowWidth * 0.8 / 8}px)`;
-  chessboard.style.gridTemplateColumns = `repeat(8, ${windowWidth * 0.8 / 8}px)`;
+  chessboard.style.gridTemplateRows = `repeat(8, ${windowSize * 0.8 / 8}px)`;
+  chessboard.style.gridTemplateColumns = `repeat(8, ${windowSize * 0.8 / 8}px)`;
 });
 
 let possibleMoves = [];
