64e99257134f259771a8de7c2f8da7094816bd60
MoCoXIII <112095580+MoCoXIII@users.noreply.github.com>
2024-12-06 13:39:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById('version').textContent = 'Version 0.2024.12.4.21.30.x';
+document.getElementById('version').textContent = 'Version 0.2024.12.6.13.30.x';
 
 window.addEventListener('load', () => {
   const windowSize = window.innerWidth < window.innerHeight ? window.innerWidth : window.innerHeight;
@@ -644,7 +644,7 @@ const handlePieceClick = (e) => {
       return;
     }
 
-    if (blockDeselectCheckBox.checked) {
+    if (blockDeselectCheckbox.checked) {
       return;
     }
 
